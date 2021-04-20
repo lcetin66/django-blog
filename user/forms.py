b80530c4e5f75334e49d7eaa118bc5dc4225ae4e
LVENT <lcetin@3ccs.com>
2021-04-20 16:59:57 +0200

    first_name,last_name,email

--- user/forms.py
+++ user/forms.py
@@ -6,6 +6,11 @@ class LoginForm(forms.Form):
     password = forms.CharField(label='Parola', widget=forms.PasswordInput)
 class RegisterForm(forms.Form):
     username = forms.CharField(max_length = 50, label='Kullanici adi')
+    #Yeni ekeme BASLANGIC
+    first_name = forms.CharField(max_length=50, label='Adi')
+    last_name = forms.CharField(max_length=50, label='Soyadi')
+    email = forms.CharField(max_length=50, label='E-Mail')
+    #Yeni ekeme BITIS
     password = forms.CharField(
         max_length=20, label='Parola', widget=forms.PasswordInput)
     confirm = forms.CharField(
@@ -13,15 +18,23 @@ class RegisterForm(forms.Form):
 
     def clean(self):
         username = self.cleaned_data.get('username')
+        #Yeni ekeme BASLANGIC
+        first_name = self.cleaned_data.get('first_name')
+        last_name = self.cleaned_data.get('last_name')
+        email = self.cleaned_data.get('email')
+        #Yeni ekeme BITIS
         password = self.cleaned_data.get('password')
         confirm = self.cleaned_data.get('confirm')
 
         if password and confirm and password != confirm:
-            raise forms.ValidationError('PArolalar eslesmiyor')
+            raise forms.ValidationError('Parolalar eslesmiyor')
 
         values = {
 
             'username': username,
+            'first_name': first_name,  # Yeni ekeme
+            'last_name': last_name,  # Yeni ekeme
+            'email': email,  # Yeni ekeme
             'password': password
         }
         return values
--- user/views.py
+++ user/views.py
@@ -1,32 +1,43 @@
-from django.shortcuts import render,redirect
-from .forms import RegisterForm,LoginForm
+from django.shortcuts import render, redirect
+from .forms import RegisterForm, LoginForm
 from django.contrib import messages
 from django.contrib.auth.models import User
-from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
-def register (request):
+
+
+def register(request):
     form = RegisterForm(request.POST or None)
-    
+
     if form.is_valid():
         username = form.cleaned_data.get('username')
+        
+        #yeni ekleme baslangic
+        first_name = form.cleaned_data.get('first_name')
+        last_name = form.cleaned_data.get('last_name')
+        email = form.cleaned_data.get('email')
+        #yeni ekleme son
+        
         password = form.cleaned_data.get('password')
 
-        newUser = User(username=username)
+        #newUser = User(username=username)
+        newUser = User(username=username,first_name=first_name,last_name=last_name,email=email)
         newUser.set_password(password)
 
         newUser.save()
         login(request, newUser)
-        messages.info(request,'Basari ile kayit oldunuz...')
+        #messages.info(request, 'Basari ile kayit oldunuz...')
+        messages.info(
+            request, f'Sn. {first_name} {last_name} basari ile kayit oldunuz...')
 
         return redirect('index')
-    
+
     context = {
         'form': form
     }
     return render(request, 'register.html', context)
-    
-    
+
 
 """ Bu Şekilde de çalışıyor
 def register(request):
@@ -55,36 +66,36 @@ def register(request):
             'form' : form
         }
         return render(request,'register.html',context)"""
-        
-        
-    
+
 
 def loginUser(request):
     form = LoginForm(request.POST or None)
-    
     context = {
         'form': form
     }
-    
+
     if form.is_valid():
         username = form.cleaned_data.get('username')
         password = form.cleaned_data.get('password')
         
-        user = authenticate(username = username,password=password)
+
+        user = authenticate(username=username, password=password)
+        
         
         if user is None:
-            messages.info(request,'Kullanici Adi veza Parola yanlis')
-            return render(request, 'login.html',context)
-        
-        messages.success(request, 'Basari ile giris yaptiniz')
-        login(request,user)
+            messages.info(request, 'Kullanici Adi veya Parola yanlis')
+            return render(request, 'login.html', context)
+
+        #messages.success(request, 'Basari ile giris yaptiniz')
+        messages.success(
+            request, f'Sn. {user.first_name} {user.last_name} basari ile giris yaptiniz...')
+        login(request, user)
         return redirect('index')
-        
-    return render(request, 'login.html',context)
+
+    return render(request, 'login.html', context)
 
 
 def logoutUser(request):
     logout(request)
-    messages.success(request,'Basari ile cikis yaptiniz')
-    return redirect ('index')
-    
+    messages.success(request, 'Basari ile cikis yaptiniz')
+    return redirect('index')
